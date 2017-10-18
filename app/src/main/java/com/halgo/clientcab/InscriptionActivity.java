668412871246046c0f843f7dbc77c75a8215b1c3
HalgoBZZ <hafedh.baazouzi@gmail.com>
2017-10-18 19:13:04 +0200

    mise a jour bla bla bye hafedh

---
+++ app/src/main/java/com/halgo/clientcab/InscriptionActivity.java
@@ -0,0 +1,16 @@
+package com.halgo.clientcab;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Halgo on 18/10/2017.
+ */
+public class InscriptionActivity extends Activity {
+
+    @Override
+    protected  void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription);
+    }
+}
