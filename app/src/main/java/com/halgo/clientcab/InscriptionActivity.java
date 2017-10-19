0625ffcedd47054e4507c05085f2724ea645359a
anouer0420 <anouerrouissi@yahoo.fr>
2017-10-19 13:08:07 +0100

    Mise à jour par Anouer ajout d'un view ,,, fait le lien entre les deux si Hafedh

--- app/src/main/java/com/halgo/clientcab/InscriptionActivity.java
+++ app/src/main/java/com/halgo/clientcab/InscriptionActivity.java
@@ -3,9 +3,7 @@ package com.halgo.clientcab;
 import android.app.Activity;
 import android.os.Bundle;
 
-/**
- * Created by Halgo on 18/10/2017.
- */
+
 public class InscriptionActivity extends Activity {
 
     @Override
