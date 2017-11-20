65369e41f85e1e5a2b2c335a88f03b486b65e912
HalgoBZZ <hafedh.baazouzi@gmail.com>
2017-11-20 16:12:48 +0100

    ok

---
+++ app/src/main/java/com/halgo/clientcab/ListRendezVousActivity.java
@@ -0,0 +1,33 @@
+package com.halgo.clientcab;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+/**
+ * Created by Halgo on 22/10/2017 nn.
+ */
+
+public class ListRendezVousActivity extends AppCompatActivity {
+
+    ListView listView ;
+    Button add;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.rendez_vous_list);
+        listView = (ListView) findViewById(R.id.list);
+        add=(Button)findViewById(R.id.addRDV);
+        add.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                startActivity(new Intent(ListRendezVousActivity.this, NouveauRendezVous.class));
+            }
+        });
+    }
+}
