a5627c8b2a7df58a023cdba9e5b45d56a945dac2
anouer0420 <anouerrouissi@yahoo.fr>
2017-11-20 16:59:10 +0100

    test

--- app/src/main/java/com/halgo/clientcab/MainActivity.java
+++ app/src/main/java/com/halgo/clientcab/MainActivity.java
@@ -14,8 +14,12 @@ import android.widget.Button;
 import android.widget.TextView;
 
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends Activity {
@@ -31,6 +35,8 @@ public class MainActivity extends Activity {
     TextInputLayout pwdLayout;
     TextView pw_oubli;
     TextView inscription;
+    String url = "http://10.0.2.2:8080/GestionCabinet/webapi/utilisateur/androidConnect/";
+    Long id=0L;
 
 
 
@@ -160,6 +166,38 @@ public class MainActivity extends Activity {
 
         // run once to disable if empty
         checkFieldsForEmptyValues();
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                final Intent intent=new Intent(MainActivity.this,ListRendezVousActivity.class);
+                String JsonURL = url + login.getText().toString()+"/"+pwd.getText().toString();
+                StringRequest obreg = new StringRequest(Request.Method.GET,JsonURL,
+                        new Response.Listener<String>(){
+                            @Override
+                            public void onResponse(String response){
+                                try {
+                                    id = Long.parseLong(response);
+                                    intent.putExtra("idCompte",id);
+                                    if(id != 0L){
+                                        startActivity(intent);
+                                    }
+                                } catch (NumberFormatException e) {
+                                    erreur.setText("barra nayek");
+                                }
+                            }
+                        },
+                        new Response.ErrorListener(){
+                            @Override
+                            public void onErrorResponse(VolleyError error){
+                                erreur.setText(error.getMessage());
+
+                            }
+                        }
+                );
+                requestQueue.add(obreg);
+            }
+        });
 
 
     }
